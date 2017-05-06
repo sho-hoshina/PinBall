1a0d0ee097b072cd7a1ef86aa05fbb7b50c2b793
sho-hoshina <i.learn.by.one.step@gmail.com>
2017-05-06 18:38:28 +0900

    PinBallプロジェクト
    発展課題を実施。

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FripperController : MonoBehaviour {
-
+    //画面サイズ
+    private float screenWidthSize = 1080.0f;
     //HingiJointコンポーネントを入れる
     private HingeJoint myHingeJoint;
 
@@ -45,6 +46,47 @@ public class FripperController : MonoBehaviour {
             SetAngle(this.defaultAngle);
         }
 
+        //タップ
+        foreach(Touch t in Input.touches)
+        {
+            var id = t.fingerId;
+
+            switch(t.phase)
+            {
+                case TouchPhase.Began:
+                    //画面左側をタップしたとき左フリッパを動かす
+                    if (t.position.x < (screenWidthSize / 2) && tag == "LeftFripperTag")
+                    {
+                        
+                        SetAngle(this.flickAngle);
+                    }
+                    //画面右側をタップしたとき右フリッパを動かす
+                    if (t.position.x >= (screenWidthSize / 2) && tag == "RightFripperTag")
+                    {
+
+                        SetAngle(this.flickAngle);
+                    }
+                    
+                    
+                    break;
+
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    //画面左側をタップしたとき左フリッパを動かす
+                    if (t.position.x < (screenWidthSize / 2) && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                    //画面右側をタップしたとき右フリッパを動かす
+                    if (t.position.x >= (screenWidthSize / 2) && tag == "RightFripperTag")
+                    {
+
+                        SetAngle(this.defaultAngle);
+                    }
+                    break;
+            }
+                
+        }
     }
 
     //フリッパーの傾きを設定
