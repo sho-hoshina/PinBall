fc5b15274d2a69c74c394b62d89d467b97246027
sho-hoshina <i.learn.by.one.step@gmail.com>
2017-05-06 10:36:15 +0900

    PinBallプロジェクト 初めてのコミット
    課題まで実施済み。(発展課題は未実施)

---
+++ Assets/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+    //ボールが見える可能性があるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverTest;
+
+    //得点
+    private int point = 0;
+
+    //得点を表示するテキスト
+    private GameObject pointText;
+
+	// Use this for initialization
+	void Start () {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverTest = GameObject.Find("GameOverText");
+        this.pointText = GameObject.Find("PointText");
+
+        this.pointText.GetComponent<Text>().text = point.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出た場合
+        if(this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverTest.GetComponent<Text>().text = "Game Over";
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("SmallCloudTag"))
+        {
+            point += 10;
+            this.pointText.GetComponent<Text>().text = point.ToString();
+        }else if(collision.gameObject.CompareTag("LargeCloudTag"))
+        {
+            point += 20;
+            this.pointText.GetComponent<Text>().text = point.ToString();
+        }
+    }
+}
